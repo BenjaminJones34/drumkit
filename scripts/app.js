44f44881e0955dccc136284743d9f8f547c23ad1
Benjamin Jones <benjamin.jones@clifton-asset.co.uk>
2021-09-09 11:41:56 +0100

    Changed some colours. Made audio pre-load

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,12 @@
-let boom = new Audio("sounds/boom.wav");
-let clap = new Audio("sounds/clap.wav");
-let hihat = new Audio("sounds/hihat.wav");
-let kick = new Audio("sounds/kick.wav");
-let openhat = new Audio("sounds/openhat.wav");
-let ride = new Audio("sounds/ride.wav");
-let snare = new Audio("sounds/snare.wav");
-let tink = new Audio("sounds/tink.wav");
-let tom = new Audio("sounds/tom.wav");
+let boom = document.querySelector("#boomSound");
+let clap = document.querySelector("#clapSound");
+let hihat = document.querySelector("#hihatSound");
+let kick = document.querySelector("#kickSound");
+let openhat = document.querySelector("#openhatSound");
+let ride = document.querySelector("#rideSound");
+let snare = document.querySelector("#snareSound");
+let tink = document.querySelector("#tinkSound");
+let tom = document.querySelector("#tomSound");
 
 const boomButton = document.querySelector("#boom");
 const clapButton = document.querySelector("#clap");
@@ -26,15 +26,17 @@ function playSound(index) {
     sounds[index][0].play();
     buttons[index].style.marginTop = "5%";
     buttons[index].style.marginLeft = "5%";
-    buttons[index].style.backgroundColor = "rgba(80, 180, 80, 0.7)";
-    buttons[index].style.boxShadow = "8.5px 16px 3.5px rgb(50, 50, 50, 0.7)";
+    buttons[index].style.backgroundColor = "rgba(50, 180, 50, 0.55)";
+    buttons[index].style.color = "rgba(245, 245, 245, 0.85)";
+    buttons[index].style.boxShadow = "8.5px 16px 3.5px rgb(tttt50, 50, 50, 0.7)";
     window.setTimeout( ()=> {
         buttons[index].style.marginTop = "0%";
         buttons[index].style.marginLeft = "0%";
-        buttons[index].style.backgroundColor = "rgba(101, 211, 101, 0.623)";
+        buttons[index].style.backgroundColor = "rgba(64, 218, 64, 0.623)";
+        buttons[index].style.color = "rgba(245, 245, 245, 1)";
         buttons[index].style.boxShadow = "10px 20px 5px rgb(50, 50, 50, 0.5)";
     
-    }, 100);
+    }, 70);
     
 };
 
